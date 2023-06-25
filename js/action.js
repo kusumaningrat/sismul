d326cbd146b85b32e1ef7c9582e4a5c9401c5a96
Kusuma16 <kusumanetcom@gmail.com>
2023-06-25 21:18:47 +0800

    Function Create Order

---
+++ js/action.js
@@ -0,0 +1,45 @@
+function selectedItem(itemName, price) {
+    document.getElementById('itemName').value = itemName;
+    document.getElementById('price').value = price;
+}
+
+function Create(e) {
+    e.preventDefault();
+
+    // Get the form value
+    const fullname = document.getElementById('fullname').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const itemName = document.getElementById('itemName').value;
+    const price = document.getElementById('price').value;
+
+    // Save Data Object
+    const data = {
+        fullname: fullname,
+        email: email,
+        phone: phone,
+        itemName: itemName,
+        price: price
+    }
+
+    const formData = JSON.parse(localStorage.getItem('data',)) || [];
+
+    if (formData) {
+        alert('Sucessfully ordered, please wait for confirmation');
+        // Push to array
+        formData.push(data)
+    }
+
+
+
+    // Save the updated array
+    const jsonArray = JSON.stringify(formData);
+    localStorage.setItem('data', jsonArray);
+
+
+    order.reset();
+    console.log(formData);
+}
+
+const order = document.getElementById('pesan');
+order.addEventListener('submit', Create);
